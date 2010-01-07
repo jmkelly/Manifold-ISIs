981e166fd44cd218a6792e20bf354f74670da6cc
James Kelly <james.kelly.99@gmail.com>
2010-01-07 13:31:34 +1100

    Add in openstreetmap licence clause

--- ImageServerOSM/Properties/AssemblyInfo.cs
+++ ImageServerOSM/Properties/AssemblyInfo.cs
@@ -16,6 +16,8 @@
 // misrepresented as being the original software.
 //
 // 3. This notice may not be removed or altered in any source distribution.
+// 4. Users of this module must adhere to the licence conditions outlined by 
+//    Openstreetmap at http://www.openstreetmap.org/
 
 
 using System.Reflection;

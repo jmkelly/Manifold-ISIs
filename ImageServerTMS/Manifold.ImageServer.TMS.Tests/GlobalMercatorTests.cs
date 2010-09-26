8a371e4a7ba3ab1e9c32ff5259c06e227805bd6a
James Kelly <james.kelly.99@gmail.com>
2010-09-26 10:16:39 +1000

    TMS and localhost ISI tests

---
+++ ImageServerTMS/Manifold.ImageServer.TMS.Tests/GlobalMercatorTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Manifold.ImageServer;
+using Manifold.ImageServer.TMS;
+
+namespace Manifold.ImageServer.TMS.Tests
+{
+    [TestFixture]
+    class GlobalMercatorTests
+    {
+        GlobalMercator globalMercator;
+        PointD latitudeLongitudePoint;
+        PointD mercatorPoint;
+
+        [SetUp]
+        public void init()
+        {
+            globalMercator = new GlobalMercator();
+            latitudeLongitudePoint = new PointD(143.068055770704, -38.4537515036619);
+            mercatorPoint = new PointD(15926263.117, -4643725.484);
+           
+        }
+
+        [Test]
+        public void TileSizeTest()
+        {
+            Assert.AreEqual(256, globalMercator.TileSize);
+        }
+
+        [Test]
+        public void InitialResolutionTest()
+        {
+            Assert.AreEqual((2 * Math.PI * 6378137) / globalMercator.TileSize, globalMercator.InitialResolution);
+        }
+        [Test]
+        public void OriginShiftTest()
+        {
+            Assert.AreEqual((2 * Math.PI * 6378137) / 2.0, globalMercator.OriginShift);
+        }
+
+        [Test]
+        public void MercatorToPixelsX()
+        {
+            Point expected = new Point(459, 197);
+            Point actual = globalMercator.GetPixels(mercatorPoint,1);
+            Assert.AreEqual(expected.X,actual.X );
+        }
+
+        [Test]
+        public void MercatorToPixelsY()
+        {
+            Point expected = new Point(459, 196);
+            Point actual = globalMercator.GetPixels(mercatorPoint, 1);
+            Assert.AreEqual(expected.Y, actual.Y);
+        }
+
+        public void ResolutionTest()
+        {
+            Double result = 2 * Math.PI * 6378137 / globalMercator.TileSize  / Math.Pow(2, 0);
+            Assert.AreEqual(result,globalMercator.Resolution(0));
+        }
+
+        [Test]
+        public void LatLonToMercatorX()
+        {
+            
+            PointD expected = new PointD(15926263.117, -4643725.484);
+            Assert.AreEqual(expected.X, Math.Round(mercatorPoint.X,3));
+           
+        }
+        [Test]
+        public void LatLonToMercatorY()
+        {
+            PointD expected = new PointD(15926263.117, -4643725.484);
+            Assert.AreEqual(expected.Y, Math.Round(mercatorPoint.Y,3));
+        }
+
+        [Test]
+        public void BitmapPointTestX()
+        {
+            Point p = new Point(459, 197);
+            PointD pM = new PointD(15926263.117, -4643725.484);
+            Assert.AreEqual(p.X, globalMercator.GetBitmapCoord(pM, 1).X);
+        }
+        [Test]
+        public void BitmapPointTestY()
+        {
+            Point p = new Point(459, 197);
+            PointD pM = new PointD(15926263.117, -4643725.484);
+            Assert.AreEqual(p.Y, globalMercator.GetBitmapCoord(pM, 1).Y);
+        }
+
+       
+
+        [Test]
+        public void TileXFromPixelTest()
+        {
+
+            Point pixelCoordinate = new Point(459, 196);
+            Point expected = new Point(1, 0);
+            Point actual = globalMercator.GetTile(pixelCoordinate);
+            Assert.AreEqual(expected.X, actual.X);
+        }
+        [Test]
+        public void TileYFromPixelTest()
+        {
+
+            Point pixelCoordinate = new Point(459, 196);
+            Point expected = new Point(1, 0);
+            Point actual = globalMercator.GetTile(pixelCoordinate);
+            Assert.AreEqual(expected.Y, actual.Y);
+        }
+    }
+}
---
+++ ImageServerTMS/Manifold.ImageServer.TMS.Tests/LocalhostServerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Manifold.ImageServer;
+using System.Xml;
+using System.IO;
+using Manifold.ImageServer.Localhost;
+
+
+namespace Manifold.ImageServer.TMS.Tests
+{
+    class LocalhostServerTest
+    {
+        
+
+        ServerLocalhost server;
+
+        [SetUp]
+        public void init()
+        {
+            server = new ServerLocalhost();
+            //TileSizeX = 256;
+            //TileSizeY = 256;
+
+            // save coordinate system in xml format
+           
+        }
+
+        [Test]
+        public void scaleLoTest()
+        {
+            int expected = 0;
+            int actual = server.ScaleLo;
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void scaleHiTest()
+        {
+            int expected = 21;
+            int actual = server.ScaleHi;
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}

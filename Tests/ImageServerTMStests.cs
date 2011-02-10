0446235a84fea33f56c9f7ab5a4e39a844470c0a
James Kelly <james.kelly.99@gmail.com>
2011-02-10 12:03:28 +1100

    change tests around

--- Tests/ImageServerTMStests.cs
+++ Tests/ImageServerTMStests.cs
@@ -7,18 +7,16 @@ using Moq;
 using ImageServerTMS;
 
 
+
 namespace Manifold.ImageServer
 {
     public class TMSTests
 
     {
-        ServerTMS TestServerTMS;
+        ServerTMS testServerTMS;
         public TMSTests()
         {
-            TestServerTMS = new ServerTMS();
-            TestServerTMS.ProxyAddress = "http://testproxy/";
-            TestServerTMS.ProxyUserName = "james";
-            TestServerTMS.ProxyPassword = "abc973";
+            testServerTMS = new ServerTMS();
         }
         
         [Fact]
@@ -28,55 +26,13 @@ namespace Manifold.ImageServer
             //mock.Setup(ServerTMS => ServerTMS.ScaleHi).Returns(19);
             //private ImageServerTMS.ServerTMS TestTMS = new ImageServerTMS.ServerTMS();
             int expected = 0;
-            int actual = TestServerTMS.ScaleLo;
+            int actual = testServerTMS.ScaleLo;
             Assert.Equal(expected, actual);
 
-        }
-        [Fact]
-        public void ScaleHiTest()
-        {
-            int expected = 19;
-            int actual = TestServerTMS.ScaleHi;
-            Assert.Equal(expected, actual);
+
+ 
         }
 
-        [Fact]
-        public void DefaultImageTypeTest()
-        {
-            string expected = ".png";
-            string actual = TestServerTMS.DefaultImageType;
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void DefaultURLTest(){
-        string expected = "http://tileserver/";
-        string actual = TestServerTMS.DefaultURL;
-        Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void ProxyAddressTest() {
-            string expected = "http://testproxy/";
-            string actual = TestServerTMS.ProxyAddress;
-            Assert.Equal(expected, actual);
-
-        }
-
-        [Fact]
-        public void ProxyUsernameTest() {
-            string expected = "james";
-            string actual = TestServerTMS.ProxyUserName ;
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void ProxyPasswordTest()
-        {
-            string expected = "abc973";
-            string actual = TestServerTMS.ProxyPassword ;
-            Assert.Equal(expected, actual); 
-        }
 
     }
 }

ffc8b46d28b967e46dd1a44472183daf4ad343b9
James Kelly <james.kelly.99@gmail.com>
2011-05-10 21:46:14 +1000

    Add GeoIQ's Hillshade acetate.

---
+++ ImageServerAcetate/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Manifold.ImageServer;
+using Manifold.ImageServer.OpenStreetMaps;
+
+namespace Manifold.ImageServer.Acetate
+{
+   
+    
+        public class AcetateHillshade : ServerOpenStreetMaps 
+        {
+            public AcetateHillshade()
+                : base(18)
+            {
+                Name = "GeoIQ Hillshade Acetate Images";
+                DefaultURL = "http://acetate.geoiq.com/tiles/hillshading";
+                ScaleNames = "0.5 m,1 m,2 m,5 m,10 m,20 m,40 m,80 m,160 m,320 m,640 m,1.3 km,2.5 km,5 km,10 km,20 km,40 km,80 km";
+            }
+        }
+    
+}
--- ImageServerOpenStreetMaps/Server.cs
+++ ImageServerOpenStreetMaps/Server.cs
@@ -388,4 +388,17 @@ namespace Manifold.ImageServer.OpenStreetMaps
         }
     }
 
+    public abstract class AcetateHillshade : ServerOpenStreetMaps 
+        {
+            public AcetateHillshade()
+                : base(18)
+            {
+                Name = "GeoIQ Hillshade Acetate Images";
+                DefaultURL = "http://acetate.geoiq.com/tiles/hillshading";
+                ScaleNames = "0.5 m,1 m,2 m,5 m,10 m,20 m,40 m,80 m,160 m,320 m,640 m,1.3 km,2.5 km,5 km,10 km,20 km,40 km,80 km";
+            }
+        }
+    
+
+
 }
